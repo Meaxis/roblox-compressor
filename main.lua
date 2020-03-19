d4fc14ac5e424f000747ae2f805020cd762ae9f8
Meaxis <37759268+Meaxis@users.noreply.github.com>
2020-03-19 01:46:52 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,14 @@
+local comp = math.random(5, 15) 
+for i = 1, comp do 
+	for i, v in pairs(game.Workspace.Model:GetChildren()) do 
+		v.Size = v.Size + Vector3.new(1, 1, 1)  
+	end
+	wait(0.1)
+end 
+wait(1) 
+for i = 1, comp do
+	for i, v in pairs(game.Workspace.Model:GetChildren()) do
+		v.Size = v.Size + Vector3.new(-1, -1, -1) 
+	end
+	wait(0.1)
+end
