8242716bb116c309683e0d6358a1728de69fefb8
Meaxis <37759268+Meaxis@users.noreply.github.com>
2020-10-23 22:37:00 +0200

    Update partcompressor.lua

--- partcompressor.lua
+++ partcompressor.lua
@@ -1,12 +1,12 @@
-local comp = math.random(5, 15) 
-for i = 1, comp do 
+local resize = math.random(5, 15) 
+for i = 1, resize do 
 	for i, v in pairs(game.Workspace.Model:GetChildren()) do 
 		v.Size = v.Size + Vector3.new(1, 1, 1)  
 	end
 	wait(0.1)
 end 
 wait(1) 
-for i = 1, comp do
+for i = 1, resize do
 	for i, v in pairs(game.Workspace.Model:GetChildren()) do
 		v.Size = v.Size + Vector3.new(-1, -1, -1) 
 	end
